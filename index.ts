58b3787f316fef997ccb91f8668c92fceab4eba9
sumukshashidhar <sumukuuu@gmail.com>
2020-11-03 22:17:51 +0530

    added a logger and some routes

--- index.ts
+++ index.ts
@@ -21,4 +21,13 @@ app.use(morgan('combined', { stream: accessLogStream }))
 
 require("./routes")(app);
 
+app.listen(process.env.PORT || 80, process.env.IP || "0.0.0.0", function (req,res ) {
+    logger.info("Server Started");
+});
+
+mongoose.connect(process.env.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    }).then(() => logger.info("MongoDB Connected")).catch((err) => console.error(err));
 
